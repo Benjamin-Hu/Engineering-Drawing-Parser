e5318fd52c27037549ea3dbdea17004bb5c17a13
Ben Hu <benjaminjhu@gmail.com>
2018-12-02 19:54:29 -0500

    Removed run.py dependency

--- wxGUI.py
+++ wxGUI.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+# Forked from wxPython
 
 import wx
+import sys
 
 try:
     from wx.lib.pdfviewer import pdfViewer, pdfButtonPanel
@@ -10,8 +11,9 @@ except ImportError:
 
 # ----------------------------------------------------------------------
 
+
 class TestPanel(wx.Panel):
-    def __init__(self, parent, log):
+    def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
         hsizer = wx.BoxSizer( wx.HORIZONTAL )
         vsizer = wx.BoxSizer( wx.VERTICAL )
@@ -69,6 +71,7 @@ class TestPanel(wx.Panel):
 
 # ---------------------------------------------------------------------
 
+
 def runTest(frame, nb, log):
     if havePyPdf:
         win = TestPanel(nb, log)
@@ -84,49 +87,40 @@ def runTest(frame, nb, log):
                            'Sorry', wx.ICON_WARNING)
         return win
 
-"""
-<html><body>
-<h2>wx.lib.pdfviewer</h2>
+# ----------------------------------------------------------------------
 
-The wx.lib.pdfviewer.pdfViewer class is derived from wx.ScrolledWindow
-and can display and print PDF files. The whole file can be scrolled from
-end to end at whatever magnification (zoom-level) is specified.
 
-<p> The viewer checks for the <b>PyMuPDF</b> then the <b>PyPDF2</b> package.
-If neither are installed an import error exception will be raised.
+class FullApp(wx.App):
+    def __init__(self):
+        wx.App.__init__(self, redirect=False)
+        self.frame = wx.Frame(None, -1, "Engineering Drawing Parser", size=(600, 500))
+        self.frame.CreateStatusBar()
 
-<p>PyMuPDF contains the Python bindings for the underlying MuPDF library, a cross platform,
-complete PDF rendering library that is GPL licenced. PyMuPDF version 1.9.2 or later is required.
+        menuBar = wx.MenuBar()
+        menu = wx.Menu()
+        item = menu.Append(wx.ID_EXIT, "Exit\tCtrl-Q", "Exit demo")
+        self.Bind(wx.EVT_MENU, self.OnExitApp, item)
+        menuBar.Append(menu, "&File")
 
-<p>Further details on PyMuPDF can be found via http://pythonhosted.org/PyMuPDF
+        self.frame.SetMenuBar(menuBar)
+        panel = TestPanel(self.frame)
+        self.frame.Show(True)
+        self.frame.Bind(wx.EVT_CLOSE, self.OnCloseFrame)
 
-<p>PyPDF2 provides a PdfFileReader class that is used to read the content stream of a PDF
-file which is subsequently rendered by the viewer itself.
-Please note that this is not a complete implementation of the pdf specification and
-will probably fail to render any random PDF file you supply. However it does seem to
-behave correctly with files that have been produced by ReportLab using Western languages.
-The main limitation is that it doesn't currently support embedded fonts.
+    def OnExitApp(self, evt):
+        self.frame.Close(True)
 
-<p>Additional details on PyPDF2 can be found via http://pythonhosted.org/PyPDF2
+    def OnCloseFrame(self, evt):
+        if hasattr(self, "window") and hasattr(self.window, "ShutdownDemo"):
+            self.window.ShutdownDemo()
+        evt.Skip()
 
-<p> There is an optional pdfButtonPanel class, derived from wx.lib.agw.buttonpanel,
-that can be placed, for example, at the top of the scrolled viewer window,
-and which contains navigation and zoom controls.
+# ----------------------------------------------------------------------
 
-<p>Alternatively you can drive the viewer from controls in your own application.
-Externally callable methods are: LoadFile, Save, Print, SetZoom, and GoPage.
-(See pdfviewer.__init__.py)
 
-<p> The viewer renders the pdf file content using Cairo if installed,
-otherwise wx.GraphicsContext is used. Printing is achieved by writing
-directly to a wx.PrintDC and using wx.Printer.
+# stuff for debugging
+print("Python %s" % sys.version)
+print("wx.version: %s" % wx.version())
 
-</body></html>
-"""
-
-#----------------------------------------------------------------------
-
-if __name__ == '__main__':
-    import sys,os
-    import run
-    run.main(['', os.path.basename(sys.argv[0])] + sys.argv[1:])
+app = FullApp()
+app.MainLoop()
